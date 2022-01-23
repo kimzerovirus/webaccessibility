5811e08fe0c1c2ff731adc5c31821d58a0f5a1f9
kimzerovirus <yg9699k@naver.com>
2022-01-24 01:13:49 +0900

    main complete

--- js/main.js
+++ js/main.js
@@ -8,3 +8,22 @@ btnCall.onclick = function(e){
     btnCall.classList.toggle("on"); 
     menuMo.classList.toggle("on"); 
 }
+
+const swiper = new Swiper("#slide", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    grabCursor : true, 
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        539:{ 
+            slidesPerView: 2,
+        },
+        1179:{  
+            slidesPerView: 3.5,
+        },
+    },
+});
